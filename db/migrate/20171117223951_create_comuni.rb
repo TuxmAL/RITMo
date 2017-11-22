7f6da36a9c8d09cce2b4190c0ca08a010e3ef63a
Antonio Liccardo <tuxmal@tiscali.it>
2017-11-22 21:14:11 +0100

    Modificata la struttura della tabella dei comuni.

--- db/migrate/20171117223951_create_comuni.rb
+++ db/migrate/20171117223951_create_comuni.rb
@@ -1,12 +1,12 @@
 class CreateComuni < ActiveRecord::Migration[5.1]
   def change
     create_table :comuni do |t|
-      t.integer :codice
+      t.string :codice
       t.string :comune
       t.string :cap
       t.string :provincia
       t.string :istat
-      t.integer :provincia_id
+      t.boolean :capoluogo
 
       t.timestamps
     end
