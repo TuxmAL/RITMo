8c87d6343ce6c459c52b99e0f52e11a5cb3215dc
Antonio Liccardo <antonio.liccardo@unina2.it>
2017-11-22 12:47:10 +0100

    Corretta la relazione d parentela con un altra anagrafica.

--- app/models/anagrafica.rb
+++ app/models/anagrafica.rb
@@ -1,7 +1,7 @@
 class Anagrafica < ApplicationRecord
   belongs_to :sesso
   belongs_to :comune, foreign_key: 'luogonascita_id'
-  belongs_to :parente
   belongs_to :parentela
+  belongs_to :parente, class_name: 'Anagrafica', foreign_key: 'parente_id', optional: true
   belongs_to :utente
 end
